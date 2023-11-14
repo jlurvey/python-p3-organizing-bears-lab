4381f621f3b881499a2b871555c38a7cd095e433
Joseph Lurvey <joeylurvey@gmail.com>
2023-11-13 20:01:17 -0500

    Complete lab

--- lib/sql_queries.py
+++ lib/sql_queries.py
@@ -6,4 +6,4 @@ select_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest = "S
 
 select_oldest_bear_and_returns_name_and_age = "SELECT name, age FROM bears ORDER BY age DESC LIMIT 1"
 
-select_youngest_bear_and_returns_name_and_age = "SELECT name, age FROM bears ORDER BY age ASC LIMIT 1"
+select_youngest_bear_and_returns_name_and_age = "SELECT name, age FROM bears ORDER BY age ASC LIMIT 1" 
